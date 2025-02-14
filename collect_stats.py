9bc3aaa1358890d335b83db20e92b8ff02fe1616
Alexander Brauckmann <alexbrauckmann@meta.com>
2025-02-14 12:24:19 -0800

    Experiment fixes

--- collect_stats.py
+++ collect_stats.py
@@ -1,3 +1,4 @@
+import os
 import numpy as np
 
 
@@ -6,26 +7,32 @@ def get_times(log, time_type):
     times = [float(line.split("time: ")[1]) for line in lines]
     return times
 
+with open("gpt-fast-stats.csv", "w") as f:
+    f.write("seqlen,time_inductor_default,time_inductor_reduce_overhead\n")
 
-# Parse times from logs
-with open("exp_outs/inductor-default/1.log", "r") as f:
-    default = f.read()
-with open("exp_outs/inductor-reduce-overhead/1.log", "r") as f:
-    reduce_overhead = f.read()
+for default_filename, reduce_overhead_filename in zip(os.listdir("exp_outs/inductor-default"), os.listdir("exp_outs/inductor-reduce-overhead")):
+    seqlen = default_filename.split(".")[0]
 
-default_times = get_times(default, "Prefill")
-reduce_overhead_times = get_times(reduce_overhead, "Prefill")
+    # Parse times from logs
+    with open("exp_outs/inductor-default/" + default_filename, "r") as f:
+        default = f.read()
+    with open("exp_outs/inductor-reduce-overhead/" + reduce_overhead_filename, "r") as f:
+        reduce_overhead = f.read()
 
-# Remove the first n iterations, which might be slower
-n = 2
-default_times = default_times[n:]
-reduce_overhead_times = reduce_overhead_times[n:]
+    default_times = get_times(default, "Prefill")
+    reduce_overhead_times = get_times(reduce_overhead, "Prefill")
 
-# Calculate metrics
-default_times_median = np.median(default_times)
-reduce_overhead_times_median = np.median(reduce_overhead_times)
+    # Remove the first n iterations, which might be slower
+    n = 2
+    default_times = default_times[n:]
+    reduce_overhead_times = reduce_overhead_times[n:]
 
-# Report metrics
-print("default median:", default_times_median)
-print("reduce_overhead median:", reduce_overhead_times_median)
-print("speedup:", default_times_median / reduce_overhead_times_median)
+    # Calculate metrics
+    default_times_median = np.median(default_times)
+    reduce_overhead_times_median = np.median(reduce_overhead_times)
+
+    print(seqlen, default_times_median, reduce_overhead_times_median)
+    #print(default_times_median / reduce_overhead_times_median)
+
+    with open("gpt-fast-stats.csv", "a") as f:
+        f.write(f"{seqlen},{default_times_median},{reduce_overhead_times_median}\n")
--- exp_default_vs_cudagraphs.sh
+++ exp_default_vs_cudagraphs.sh
@@ -4,28 +4,31 @@ set -e
 
 # Common variables
 OUT_DIR=exp_outs
-
 mkdir -p $OUT_DIR
-
 mkdir -p $OUT_DIR/inductor-default
 mkdir -p $OUT_DIR/inductor-reduce-overhead
-
 MODEL_REPO=meta-llama/Llama-2-7b-chat-hf
+
+export CUDA_VISIBLE_DEVIES=3
 
 PROMPT='Models for speech recognition or for NLP are often trained on input tensors with variable sequence length. Variable length can be problematic for PyTorch caching allocator and can lead to reduced performance or to unexpected out-of-memory errors. If a batch with a short sequence length is followed by an another batch with longer sequence length, then PyTorch is forced to release intermediate buffers from previous iteration and to re-allocate new buffers. This process is time consuming and causes fragmentation in the caching allocator which may result in out-of-memory errors.'
 
 # Run experiments
 # ##################
 # Run
-for i in {1..1}; do
+for WORD_COUNT in {1..100}; do
+    echo "Processing length $WORD_COUNT"
+    echo "-----------------------------"
+
+    # Extract the first WORD_COUNT words from the PROMPT
+    CURRENT_PROMPT=$(echo $PROMPT | awk '{for(i=1;i<='$WORD_COUNT';i++) printf $i" "; print ""}')
+    echo $CURRENT_PROMPT
     # Default
-    python generate.py --compile --checkpoint_path checkpoints/$MODEL_REPO/model.pth --prompt "$PROMPT" --compile_prefill --num_samples 100 \
-    &> $OUT_DIR/inductor-default/$i.log
-
+    python generate.py --compile --checkpoint_path checkpoints/$MODEL_REPO/model.pth --prompt "$CURRENT_PROMPT" --compile_prefill --num_samples 50 \
+    &> $OUT_DIR/inductor-default/$WORD_COUNT.log
     # Reduce overhead
-    python generate.py --compile --checkpoint_path checkpoints/$MODEL_REPO/model.pth --prompt "$PROMPT" --compile_prefill_reduce_overhead --num_samples 100 \
-    &> $OUT_DIR/inductor-reduce-overhead/$i.log
+    python generate.py --compile --checkpoint_path checkpoints/$MODEL_REPO/model.pth --prompt "$CURRENT_PROMPT" --compile_prefill_reduce_overhead --num_samples 50 \
+    &> $OUT_DIR/inductor-reduce-overhead/$WORD_COUNT.log
 done
-
 # Aggregate Results
 python collect_stats.py
